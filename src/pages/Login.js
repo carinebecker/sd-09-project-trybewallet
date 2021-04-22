ece4ee9bb3cdb4b942da4cdf006c2ba27e9a2ac5
Pedro Amaral <amaralph1995@gmail.com>
2021-04-22 19:44:01 -0300

    Cria inputs e botão da tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import '../App.css';
 
 class Login extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      btnAvaliable: false,
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <section className="login-section">
+        <label>Email
+          <input
+            type="text"
+            testid="email-input"
+            required
+          />
+        </label>
+        <label>Senha
+          <input
+            type="password"
+            data-testid="password-input"
+            required
+          />
+        </label>
+        <label>
+          <button
+            type="button"
+            className="login-button"
+          >
+            Entrar
+          </button>
+        </label>
+      </section>
+    );
   }
 }
 
