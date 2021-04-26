405aab8d694d7a62b9150d3b9857200cfbf04c75
Andre <dehhmenezes87@gmail.com>
2021-04-26 19:47:54 -0300

    Wallet.css deleted

--- src/pages/Wallet/index.js
+++ src/pages/Wallet/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../../components/Header';
 import ExpensesForm from '../../components/ExpenseForm';
 import ExpenseTable from '../../components/ExpenseTable';
-import './style.css';
 
 class Wallet extends React.Component {
   render() {
