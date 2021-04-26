92f91bf5bdbbf5d8ad91412843073e54b2110591
Lucas Silva <martssilvapro@gmail.com>
2021-04-26 11:16:24 -0300

    correcao lint

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -126,9 +126,9 @@ class ExpenseForm extends React.Component {
         data-testid="method-input"
         onChange={ this.formFieldsControl }
       >
-        <option value="dinheiro">Dinheiro</option>
-        <option value="cartaoCredito">Cartão de crédito</option>
-        <option value="cartaoDebito">Cartão de débito</option>
+        <option value="Dinheiro">Dinheiro</option>
+        <option value="Cartão de crédito">Cartão de crédito</option>
+        <option value="Cartão de débito">Cartão de débito</option>
       </select>
     );
   }
@@ -143,11 +143,11 @@ class ExpenseForm extends React.Component {
         data-testid="tag-input"
         onChange={ this.formFieldsControl }
       >
-        <option value={ alimentacao }>Alimentação</option>
-        <option value="lazer">Lazer</option>
-        <option value="trabalho">Trabalho</option>
-        <option value="transporte">Transporte</option>
-        <option value="saude">Saúde</option>
+        <option value="Alimentação">Alimentação</option>
+        <option value="Lazer">Lazer</option>
+        <option value="Trabalho">Trabalho</option>
+        <option value="Transporte">Transporte</option>
+        <option value="Saúde">Saúde</option>
       </select>
     );
   }
