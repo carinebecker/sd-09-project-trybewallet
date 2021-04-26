417469428224883d7b082c0e50477afc52b4095a
Leonnardo Lair <leonnardo0316@gmail.com>
2021-04-26 16:23:39 -0300

    fixes

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,30 +1,29 @@
 const INITIAL_STATE = {
-    loading: true,
-    firstExpense: false,
-    id: 0,
-    currencies: [],
-    expenses: [],
-  };
-  
-  export default function reducer(state = INITIAL_STATE, action) {
-    const { type, payload } = action;
-    switch (type) {
-    case 'REQUEST_DATA':
-      return { ...state, loading: true };
-    case 'RECEIVE_DATA':
-      return { ...state,
-        loading: false,
-        currencies: [payload],
-      };
-    case 'NEW_EXPENSE':
-      return { ...state,
-        id: state.id + 1,
-        expenses: [...state.expenses, payload],
-        firstExpense: true,
-      };
-  
-    default:
-      return state;
-    }
+  loading: true,
+  firstExpense: false,
+  id: 0,
+  currencies: [],
+  expenses: [],
+};
+
+export default function reducer(state = INITIAL_STATE, action) {
+  const { type, payload } = action;
+  switch (type) {
+  case 'REQUEST_DATA':
+    return { ...state, loading: true };
+  case 'RECEIVE_DATA':
+    return { ...state,
+      loading: false,
+      currencies: [payload],
+    };
+  case 'NEW_EXPENSE':
+    return { ...state,
+      id: state.id + 1,
+      expenses: [...state.expenses, payload],
+      firstExpense: true,
+    };
+
+  default:
+    return state;
   }
-  
+}
