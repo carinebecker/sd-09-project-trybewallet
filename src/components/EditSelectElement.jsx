7604aaf915f94bc6007e05c9ea30c8efddbabbac
Perycles reis <perycles_reis@hotmail.com>
2021-05-04 17:51:00 -0300

    requisito bonus

--- src/components/EditSelectElement.jsx
+++ src/components/EditSelectElement.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Moedas from '../services/requireApi';
 import { editExpenseAction, editingOldElement } from '../actions';
 
 class EditSelectElement extends React.Component {
