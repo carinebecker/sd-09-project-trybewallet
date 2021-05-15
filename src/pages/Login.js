4c6b2740e3ce29e4f9560c99014cc4d717b02c36
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-05-14 22:55:19 -0300

    Commit 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,42 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <span>Login</span>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          onChange={ this.handleChange }
+          value={ email }
+        />
+        <input
+          type="text"
+          data-testid="password-input"
+          name="password"
+          onChange={ this.handleChange }
+          value={ password }
+        />
+      </div>
+    );
   }
 }
 
