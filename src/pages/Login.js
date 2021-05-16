d7cd0ebb7371d14d03f4930d593eb52cdafae41c
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-05-16 17:21:43 -0300

    resolvendo requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { email: '', password: '' };
+  }
+
+  handleChange({ target: { type, value } }) {
+    this.setState({ [type]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <div>
+          <input
+            type="email"
+            data-testid="email-input"
+            value={ email }
+            onClick={ this.handleChange }
+            required
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            minLength="6"
+            value={ password }
+            onClick={ this.handleChange }
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          onClick={ () => (<Redirect to="/carteira" />) }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,20 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = { currencies: [], expenses: [], edit: false, expense: {} };
+
+export default function wallet(state = initialState, action) {
+  switch (action.type) {
+  case 'SET_CURRENCIES':
+    return { ...state, currencies: action.currencies };
+
+  case 'SET_EXPENSES':
+    return { ...state, expenses: action.expenses };
+
+  case 'SET_EXPENSE':
+    return { ...state, expenses: [...state.expenses, action.expense] };
+
+  case 'SET_EDIT':
+    return { ...state, edit: action.condition, expense: action.expense };
+
+  default:
+    return state;
+  }
+}
