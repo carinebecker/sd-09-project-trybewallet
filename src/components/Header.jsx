a7acc9fab5cb1cc7ec5c12cc588c666f7bc1620c
Kevin Fraga <kevinciolac@gmail.com>
2021-05-22 14:22:28 -0300

    :four: NewExpense

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { string } from 'prop-types';
-import '../styles/header.css';
+import { string, number } from 'prop-types';
 
 class Header extends Component {
   render() {
-    const { email } = this.props;
+    const { email, total } = this.props;
+    const init = 0;
     return (
-      <header>
-        <h2 className="header-title" data-testid="email-field">{ email }</h2>
-        <p className="header-field" data-testid="total-field">0</p>
-        <p className="header-field" data-testid="header-currency-field">BRL</p>
+      <header className="flexbox">
+        <h2 className="header-title" data-testid="email-field">
+          {email}
+        </h2>
+        <p className="header-field" data-testid="total-field">
+          {!total ? init.toFixed(2) : total.toFixed(2)}
+        </p>
+        <p className="header-field" data-testid="header-currency-field">
+          BRL
+        </p>
       </header>
     );
   }
@@ -18,10 +24,12 @@ class Header extends Component {
 
 Header.propTypes = {
   email: string,
+  total: number,
 }.isRequired;
 
-const mapStateToProps = ({ user: { email } }) => ({
+const mapStateToProps = ({ user: { email }, wallet: { total } }) => ({
   email,
+  total,
 });
 
 export default connect(mapStateToProps, null)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import NewExpense from '../components/NewExpense';
+import '../styles/wallet.css';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        TrybeWallet
+        <NewExpense />
       </div>
     );
   }
