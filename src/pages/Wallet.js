11fe5fd3690ce9785bdd4494d4c86f8d1bfb84da
Matheus Monteiro <monteiromsh@gmail.com>
2021-05-23 01:27:56 -0300

    delete de componentes quebrados

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import Header from './Header';
-import WalletForm from './WalletForm';
 
 class Wallet extends React.Component {
   render() {
-    return (
-      <div>
-        <Header />
-        <WalletForm />
-      </div>);
+    return <div>Wallet</div>;
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,18 +1,1 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
-import { USER_EMAIL } from '../actions';
-
-const INITIAL_STATE = {
-  email: '',
-};
-
-export default function loginReducer(state = INITIAL_STATE, action) {
-  switch (action.type) {
-  case USER_EMAIL:
-    return {
-      ...state,
-      email: action.email,
-    };
-  default:
-    return state;
-  }
-}
