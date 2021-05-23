ff69eb0ec9afc3c89a240627bb281c616346e089
Matheus Monteiro <monteiromsh@gmail.com>
2021-05-23 01:28:43 -0300

    correção em index.js quebrado

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'),
-  <Provider store={ store }>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
 );
 
 // If you want your app to work offline and load faster, you can change
