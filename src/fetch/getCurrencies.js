2019281617f16c6114684e0dbfb79acf97aad08a
unknown <nicolasgodek@gmail.com>
2021-05-25 03:39:52 -0300

    fix

--- src/fetch/getCurrencies.js
+++ src/fetch/getCurrencies.js
@@ -1,5 +1,5 @@
 const getCurrencies = async () => {
-	const endpoint = 'https://economia.awesomeapi.com.br/json/all';
+  const endpoint = 'https://economia.awesomeapi.com.br/json/all';
   const request = await fetch(endpoint);
   const response = request.json();
   return response;
