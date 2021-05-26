0001f9ee374325dd147001a702a48659385c9daf
Samuel <samuelvanguarda@gmail.com>
2021-05-26 13:51:13 -0300

    Requisito 5

---
+++ src/components/Table.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Table extends Component {
+  constructor() {
+    super();
+    this.createInfoTable = this.createInfoTable.bind(this);
+  }
+
+  createInfoTable() {
+    const { expenses } = this.props;
+    if (expenses) {
+      return (
+        expenses.map((row) => (
+          <tr key={ row.id }>
+            <td>{ row.description }</td>
+            <td>{ row.tag }</td>
+            <td>{ row.method }</td>
+            <td>{ row.value }</td>
+            <td>{ row.exchangeRates[row.currency].name }</td>
+            <td>{ Number(row.exchangeRates[row.currency].ask).toFixed(2) }</td>
+            <td>
+              { Number(row.exchangeRates[row.currency].ask * row.value).toFixed(2) }
+            </td>
+            <td>Real</td>
+          </tr>
+        ))
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+            { this.createInfoTable() }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(Object).isRequired,
+};
