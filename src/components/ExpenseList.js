9514103dc6a02ea50709f7b55c1988d56896f489
Rodrigo <deltadigo@gmail.com>
2021-05-27 01:40:30 -0300

    Requisito 5

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropType from 'prop-types';
 
 class ExpenseList extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <h1>Expense List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ expense.value }</td>
+              <td>{ expense.exchangeRates[expense.currency].name }</td>
+              <td>{ parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
+              <td>
+                { (
+                  expense.value * expense.exchangeRates[expense.currency].ask
+                ).toFixed(2) }
+              </td>
+              <td>Real</td>
+              <td>editandremove</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
 
-export default ExpenseList;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+ExpenseList.propTypes = {
+  expenses: PropType.arrayOf(Object),
+}.isRequired;
+
+export default connect(mapStateToProps, null)(ExpenseList);
